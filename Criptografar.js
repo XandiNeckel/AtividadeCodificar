b3bf470ea3b954d46523b84483c1cb654ea99bca
XandiNeckel <barra_xandi@hotmail.com>
2022-04-04 22:46:52 -0300

    inserindo conteudos html,css

---
+++ Criptografar.js
@@ -0,0 +1,10 @@
+var letras  = "abcdefghijklmnopqrstuvxwyz"; // letras alfabeto
+
+var letraEscolhida = letras[i];  // escolher a letra desejada
+
+// colocar a letra na tela [concatena igual]
+document.write(letraEscolhida + ' = ');
+
+// charcode 
+document.write(letraEscolhida.charCodeAt());
+
